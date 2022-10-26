bcf1c428589b8a5c060cad88705e3a0292051c3a
Alina <yudkev@gmail.com>
2022-10-26 14:16:03 -0400

    changed deploy path

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yudkev/mapp2.git main:gh-pages
+git push -f git@github.com:yudkev/compas.git main:gh-pages
 
 cd -
