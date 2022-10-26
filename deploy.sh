44bafe41d8a6ec28b423ddf540256e2639a0353f
Alina <yudkev@gmail.com>
2022-10-26 14:31:26 -0400

    changed ssh

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yudkev/compas.git main:gh-pages
+git push -f https://github.com/yudkev/compas.git main:gh-pages
 
 cd -
